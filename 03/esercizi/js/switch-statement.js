6cf30fa7a566da5a008b916285ec18d26b210b19
Colangelo <Pietro.Colangelo@fao.org>
2017-03-29 11:04:51 +0200

    esercizi

---
+++ 03/esercizi/js/switch-statement.js
@@ -0,0 +1,24 @@
+var msg;        // Message
+var level = 2;  // Level
+
+// Determine message based on level
+switch (level) {
+case 1:
+    msg = 'Primo Livello - Buona Fortuna';
+    break;
+
+case 2:
+    msg = 'Secondo dei tre - Andiamo!';
+    break;
+
+case 3:
+    msg = 'Terzo livello - Ottimo!';
+    break;
+
+default:
+    msg = 'Buona Fortuna!';
+    break;
+}
+
+var el = document.getElementById('answer');
+el.textContent = msg;
