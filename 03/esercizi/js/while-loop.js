8211f82373088010ee2d594614c06680a005bfb1
Pietro Colangelo <pcolangelo80@gmail.com>
2017-03-29 10:50:32 +0200

    new commit

--- 03/esercizi/js/while-loop.js
+++ 03/esercizi/js/while-loop.js
@@ -1,1 +1,12 @@
-var i = 1;var msg = '';while (i < 10) {  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;}document.getElementById('answer').innerHTML = msg;var i = 1;var msg = '';while (i < 10) {  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;}document.getElementById('answer').innerHTML = msg;
+var i = 1;
+var msg = '';
+
+
+while (i < 10) {
+  msg += i + ' x 5 = ' + (i * 5) + '<br />';
+  i++;
+}
+
+
+document.getElementById('answer').innerHTML = msg;
+var i = 1;var msg = '';while (i < 10) {  msg += i + ' x 5 = ' + (i * 5) + '<br />';  i++;}document.getElementById('answer').innerHTML = msg;
